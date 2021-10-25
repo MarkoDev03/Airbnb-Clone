84cf505de4a4ba01791770da2651fe9f34474b7f
MarkoDev03 <mmarko.perovici3@gmail.com>
2021-10-25 13:21:40 +0200

    15.0.0

--- components/Header.jsx
+++ components/Header.jsx
@@ -50,7 +50,7 @@ function Header({ placeholder }) {
   }
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md  px-1 md:p-3 md:px-10">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-1 px-1 md:p-3 md:px-10">
       <div 
       onClick={() => router.push('/')}
       className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -74,9 +74,9 @@ function Header({ placeholder }) {
         <div className="flex md:hidden md:auto mr-2  w-auto md:col-span-1 md:space-x-4 items-center justify-end text-gray-500 ">
         <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
         <GlobeAltIcon className="hidden md:inline-flex h-5" />
-        <div className="flex items-center space-x-2 border-2 p-1 rounded-full bg-white">
+        <div className="flex items-center space-x-1 border-2 p-1 rounded-full bg-white">
           <MenuIcon className="h-5" />
-          <UserCircleIcon className="h-5" />
+          <UserCircleIcon className="h-6" />
         </div>
       </div>
 
