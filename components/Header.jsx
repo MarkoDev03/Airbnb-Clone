acb3679253f81cd1a648fba51b4e50ccb5a7fe64
MarkoDev03 <mmarko.perovici3@gmail.com>
2021-10-25 13:16:37 +0200

    final

--- components/Header.jsx
+++ components/Header.jsx
@@ -50,7 +50,7 @@ function Header({ placeholder }) {
   }
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md  px-3 md:p-3 md:px-10">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md  px-1 md:p-3 md:px-10">
       <div 
       onClick={() => router.push('/')}
       className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -62,7 +62,7 @@ function Header({ placeholder }) {
         />
       </div>
 
-      <div className="flex col-span-2 md:col-span-1 m-2 justify-center items-center md:border-2 rounded-full py-1 md:shadow-sm text-gray-600 text-sm placeholder-gray-400">
+      <div className="flex col-span-2 md:col-span-1 m-2 justify-center items-center md:border-2 rounded-full md:py-1 md:shadow-sm text-gray-600 text-sm placeholder-gray-400">
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
@@ -71,12 +71,12 @@ function Header({ placeholder }) {
           className="md:flex-grow md:pl-5 bg-transparent outline-none text-sm text-center md:text-left"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 " />
-        <div className="flex md:hidden md:auto  w-auto md:col-span-1 md:space-x-4 items-center justify-end text-gray-500 ">
+        <div className="flex md:hidden md:auto mr-2  w-auto md:col-span-1 md:space-x-4 items-center justify-end text-gray-500 ">
         <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
-        <GlobeAltIcon className="hidden md:inline-flex h-6" />
-        <div className="flex items-center space-x-2 border-2 p-2 rounded-full bg-white">
-          <MenuIcon className="h-6" />
-          <UserCircleIcon className="h-6" />
+        <GlobeAltIcon className="hidden md:inline-flex h-5" />
+        <div className="flex items-center space-x-2 border-2 p-1 rounded-full bg-white">
+          <MenuIcon className="h-5" />
+          <UserCircleIcon className="h-5" />
         </div>
       </div>
 
